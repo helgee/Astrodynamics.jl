70cf2f66c940ec3e662e7c1562acdad491491208
Helge Eichhorn <git@helgeeichhorn.de>
2016-02-15 17:58:45 +0100

    First steps for state vector implementation.

--- src/Astrodynamics.jl
+++ src/Astrodynamics.jl
@@ -1,38 +1,23 @@
 module Astrodynamics
 
-    using Datetime
+#= using Base.Dates =#
+#= using Dopri =#
+#=  =#
+#= export State =#
+#= export JulianDate, JD2000, JD1950, MJD, jd2000, jd1950, mjd =#
+#= export ecctomean, meantoecc, ecctotrue, truetoecc =#
+#= export propagate =#
+#= export planets =#
 
-    importall Base
+include("time.jl")
+include("planets.jl")
+include("frames.jl")
+include("states.jl")
+include("math.jl")
+include("kepler.jl")
+include("propagator.jl")
 
-    export State
-    export julian, gregorian, JD2000, JD1950, MJD
-    export ecctomean, meantoecc, ecctotrue, truetoecc
-    export propagate
-    export planets
+#= iss = State([8.59072560e+02, -4.13720368e+03, 5.29556871e+03, 7.37289205e+00, 2.08223573e+00, 4.39999794e-01], =#
+#=     DateTime(2013,3,18,12,0,0), "eci", "earth") =#
 
-    immutable State
-        rv::Vector
-        t::DateTime
-        frame::String
-        body::String
-    end
-    
-    include("constants.jl")
-    include("time.jl")
-    include("frames.jl")
-    include("math.jl")
-    include("kepler.jl")
-
-    iss = State([8.59072560e+02, -4.13720368e+03, 5.29556871e+03, 7.37289205e+00, 2.08223573e+00, 4.39999794e-01],
-        datetime(2013,3,18,12,0,0), "eci", "earth")
-
-    function propagate(s::State; method::String="kepler")
-       mu = planets[s.body]["mu"] 
-       if method == "kepler"
-           ele = elements(s.rv, mu)
-           tend = period(ele[1], mu)
-           dt = [0:tend]
-           return cartesian(kepler(ele, dt, mu), mu)
-       end
-    end
 end
---
+++ src/states.jl
@@ -0,0 +1,28 @@
+abstract AbstractState
+
+immutable State{T} <: AbstractState
+    rv::Vector{Float64}
+    epoch::AbstractEpoch
+    body::AbstractBody
+end
+
+type StateSpace <: AbstractState
+end
+
+typealias ECEFState State{ECEF}
+typealias ECIState State{ECI}
+
+function elements(s::State)
+    return elements(s.rv, planets[s.body]["mu"])
+end
+
+function elements(s::State, deg::Bool)
+    ele = elements(s)
+    if deg
+        ele[3:end] = ele[3:end]*180/pi
+        return ele
+    else
+        return ele
+    end
+end
+
