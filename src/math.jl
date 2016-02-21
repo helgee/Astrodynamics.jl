017c316be9b6486fac56f80ac9873726d9fbc85b
Helge Eichhorn <git@helgeeichhorn.de>
2016-02-21 16:21:13 +0100

    Add rotation matrices.

--- src/math.jl
+++ src/math.jl
@@ -10,3 +10,33 @@ function newton(x0::Float64, func::Function, derivative::Function, maxiter::Int=
     end
     error("Not converged.")
 end
+
+function rotate_x(angle::Float64)
+    mat = zeros(3, 3)
+    mat[1,1] = 1
+    mat[2,2] = cos(angle)
+    mat[2,3] = sin(angle)
+    mat[3,2] = -sin(angle)
+    mat[3,3] = cos(angle)
+    return mat
+end
+
+function rotate_y(angle::Float64)
+    mat = zeros(3, 3)
+    mat[1,1] = cos(angle)
+    mat[1,3] = sin(angle)
+    mat[2,2] = 1
+    mat[3,1] = -sin(angle)
+    mat[3,3] = cos(angle)
+    return mat
+end
+
+function rotate_z(angle::Float64)
+    mat = zeros(3, 3)
+    mat[1,1] = cos(angle)
+    mat[1,2] = sin(angle)
+    mat[2,1] = -sin(angle)
+    mat[2,2] = cos(angle)
+    mat[3,3] = 1
+    return mat
+end
