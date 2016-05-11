345aac511c11bcd06bc8fb2dbd8e52e13f651e10
Helge Eichhorn <git@helgeeichhorn.de>
2016-05-11 09:12:06 +0200

    Experiments with state vectors and frames.

--- src/states.jl
+++ src/states.jl
@@ -4,8 +4,8 @@ abstract AbstractState
 
 immutable State{T<:Frame, S<:Timescale} <: AbstractState
     frame::Type{T}
-    rv::Vector{Float64}
     epoch::Epoch{S}
+    rv::Vector{Float64}
     body::Body
 end
 
@@ -14,25 +14,19 @@ Base.eltype{T,S}(::Type{State{T,S}}) = T
 body(s::State) = s.body
 rv(s::State) = s.rv
 epoch(s::State) = s.epoch
-frame(s::State) = s.frame
+reference_frame(s::State) = s.frame
+
+elements(s::State) = elements(rv(s), μ(body(s)))
 
 type StateSpace <: AbstractState
 end
 
-typealias ECEFState State{ECEF}
-typealias ECIState State{ECI}
-
-function elements(s::State)
-    return elements(rv(s), μ(body(s)))
-end
-
-function elements(s::State, deg::Bool)
-    ele = elements(s)
-    if deg
-        ele[3:end] = ele[3:end]*180/pi
-        return ele
-    else
-        return ele
+for frame in FRAMES
+    sym = symbol(frame, "State")
+    @eval begin
+        typealias $(sym) State{$frame}
+        export $sym
     end
 end
-
+# Constructor for typealiases
+Base.call{T<:Frame}(::Type{State{T}}, args...; kwargs...) = State(T, args...; kwargs...)
