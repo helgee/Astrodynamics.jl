7a1c265a576e72a926564121f052338528c78884
Helge Eichhorn <git@helgeeichhorn.de>
2016-05-12 16:40:13 +0200

    Add single rotation axis.

--- src/rotations.jl
+++ src/rotations.jl
@@ -1,7 +1,11 @@
-export rotation_order
+export rotation_axes
 
-function rotation_order(ord::AbstractString)
-    reg = r"^[xzy]{3}$|^[123]{3}$"
+function rotation_axes(ord::AbstractString)
+    if !(length(ord) == 1 || length(ord) == 3)
+        throw(ArgumentError("Please provide either a triple of axes or a single rotation axis."))
+    end
+
+    reg = r"^[xzy]{1,3}$|^[123]{1,3}$"
 
     if !ismatch(reg, lowercase(ord))
         throw(ArgumentError("Rotation axes must be indicated as either X, Y, Z or 1, 2, 3."))
@@ -9,10 +13,10 @@ function rotation_order(ord::AbstractString)
 
     order = replace(replace(replace(lowercase(ord), "x", "1"), "y", "2"), "z", "3")
 
-    if order[1] == order[2] || order[2] == order[3]
+    if length(order) == 3 && (order[1] == order[2] || order[2] == order[3])
         throw(ArgumentError("Subsequent rotations around the same axis are meaningless."))
     end
     parse(Int, order)
 end
 
-rotation_order(ord::Int) = rotation_order(string(ord))
+rotation_axes(ord::Int) = rotation_axes(string(ord))
--- test/rotations.jl
+++ test/rotations.jl
@@ -1,13 +1,21 @@
 @testset "Rotations" begin
-    @test_throws ArgumentError rotation_order("XYP")
-    @test_throws ArgumentError rotation_order("XY1")
-    @test_throws ArgumentError rotation_order("124")
-    @test_throws ArgumentError rotation_order("1231")
-    @test_throws ArgumentError rotation_order("XYZX")
-    @test_throws ArgumentError rotation_order("XXZ")
-    @test_throws ArgumentError rotation_order("ZXX")
-    @test rotation_order("XYZ") == 123
-    @test rotation_order("xyz") == 123
-    @test rotation_order("123") == 123
-    @test rotation_order(123) == 123
+    @test_throws ArgumentError rotation_axes("XYP")
+    @test_throws ArgumentError rotation_axes("XY1")
+    @test_throws ArgumentError rotation_axes("124")
+    @test_throws ArgumentError rotation_axes("1231")
+    @test_throws ArgumentError rotation_axes("XYZX")
+    @test_throws ArgumentError rotation_axes("XXZ")
+    @test_throws ArgumentError rotation_axes("ZXX")
+    @test rotation_axes("XYZ") == 123
+    @test rotation_axes("xyz") == 123
+    @test rotation_axes("123") == 123
+    @test rotation_axes(123) == 123
+
+    @test rotation_axes("X") == 1
+    @test rotation_axes("Y") == 2
+    @test rotation_axes("Z") == 3
+    @test rotation_axes(3) == 3
+    @test_throws ArgumentError rotation_axes("B")
+    @test_throws ArgumentError rotation_axes("4")
+    @test_throws ArgumentError rotation_axes("XY")
 end
