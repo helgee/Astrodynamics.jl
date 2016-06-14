5e760d5b5ede8a53543aa530252250c00ba11a2d
Helge Eichhorn <git@helgeeichhorn.de>
2016-06-14 17:32:40 +0200

    Add example based on GMAT Hohmann transfer example.

---
+++ examples/hohmann.jl
@@ -0,0 +1,39 @@
+using Astrodynamics
+
+s0 = State(
+    Epoch(TAI, 2000, 1, 1, 12),
+    [7100, 0, 1300],
+    [0, 7.35, 1],
+)
+
+toi = @vary(
+    toi = 1e-4,
+    toi >= 0.0,
+    toi <= pi,
+    ImpulsiveManeuver(along=toi)
+)
+goi = @vary(
+    goi = 0,
+    goi >= 0.0,
+    goi <= pi,
+    ImpulsiveManeuver(along=goi)
+)
+
+ode = ODE(
+    gravity=J2Gravity(Earth),
+    maxstep=2700,
+    discontinuities = [
+        Discontinuity(PericenterEvent(), toi),
+        Discontinuity(ApocenterEvent(), goi),
+    ],
+)
+
+seg = Segment(
+    dt=86400,
+    start = InitialOrbit(s0),
+    stop = TargetOrbit(
+        sma = 42165.0,
+        ecc = 0.0,
+    ),
+    propagator = ode,
+)
