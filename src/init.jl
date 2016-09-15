b1fcb07fb12ba24877523d192fe9cd9cbcce41ba
Helge Eichhorn <git@helgeeichhorn.de>
2016-09-15 22:51:58 +0200

    Update leap seconds kernel.

--- src/init.jl
+++ src/init.jl
@@ -12,7 +12,7 @@ end
 const PATH = normpath(joinpath(splitdir(@__FILE__)[1],"..","data"))
 const DATA_FILES = Dict(
     :ephemeris  =>  Dict(:name => "DE430 ephemeris", :url => "http://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/de430.bsp", :path => joinpath(PATH, "de430.bsp")),
-    :leapseconds  =>  Dict(:name => "leap seconds", :url => "http://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0011.tls", :path => joinpath(PATH, "naif0011.tls")),
+    :leapseconds  =>  Dict(:name => "leap seconds", :url => "http://naif.jpl.nasa.gov/pub/naif/generic_kernels/lsk/naif0012.tls", :path => joinpath(PATH, "naif0012.tls")),
     :iau1980  =>  Dict(:name => "IAU1980 Earth orientation", :url => "http://maia.usno.navy.mil/ser7/finals.all", :path => joinpath(PATH, "finals.all")),
     :iau2000  =>  Dict(:name => "IAU2000 Earth orientation", :url => "http://maia.usno.navy.mil/ser7/finals2000A.all", :path => joinpath(PATH, "finals2000A.all")),
 )
