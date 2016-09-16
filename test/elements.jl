b7af7509f3f94430b0b7e16ee985042a565525f4
Helge Eichhorn <git@helgeeichhorn.de>
2016-09-16 02:47:03 +0200

    Fix deprecations.

--- test/elements.jl
+++ test/elements.jl
@@ -4,7 +4,7 @@
     rexp = [6524.834, 6862.875, 6448.296]
     vexp = [4.901327, 5.533756, 1.976341]
     elexp = [36127.343, 0.832853, 87.870, 227.898, 53.38, 92.355]
-    elexp[3:end] = deg2rad(elexp[3:end])
+    elexp[3:end] = deg2rad.(elexp[3:end])
     el = keplerian(rexp, vexp, μ)
     @test isapprox(el, elexp, rtol=1e-3)
     r, v = cartesian(el, μ)
