7cd9e6313a5171210dec9ae37b2f1136e841ca66
Helge Eichhorn <git@helgeeichhorn.de>
2017-03-13 17:05:54 +0100

    Start putting things back together.

--- src/Astrodynamics.jl
+++ src/Astrodynamics.jl
@@ -2,27 +2,11 @@
 
 module Astrodynamics
 
-#= include("parameters.jl") =#
-#= include("time.jl") =#
-#= include("util.jl") =#
-#= include("iers.jl") =#
-#= include("init.jl") =#
-#= include("bodies.jl") =#
-#= include("librationpoints.jl") =#
-#= include("elements.jl") =#
-#= include("planets.jl") =#
-#= include("satellites.jl") =#
-#= include("states.jl") =#
-#= include("events.jl") =#
-#= include("trajectories.jl") =#
-#= include("propagators.jl") =#
-#= include("thirdbody.jl") =#
-#= include("gravity.jl") =#
-#= include("math.jl") =#
-#= include("kepler.jl") =#
-#= include("rotations.jl") =#
-#= include("stumpff.jl") =#
-#= include("mission.jl") =#
-#= include("propagators/ode2.jl") =#
+using Reexport
+
+@reexport using AstroDynBase
+@reexport using AstroDynIO
+@reexport using AstroDynPlots
+@reexport using AstroDynModels
 
 end
