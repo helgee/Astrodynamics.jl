b25dbef1aced85b29fe3642845d60d920b4105a9
autodocs <autodocs>
2017-03-14 07:41:08 +0000

    build based on 7abf084

--- latest/search_index.js
+++ latest/search_index.js
@@ -17,11 +17,19 @@ var documenterSearchIndex = {"docs": [
 },
 
 {
+    "location": "api/time.html#AstronomicalTime.Epoch",
+    "page": "Time",
+    "title": "AstronomicalTime.Epoch",
+    "category": "Constant",
+    "text": "Epoch{T}(year, month, day, hour=0, minute=0, seconds=0, milliseconds=0) where T<:Timescale\n\nConstruct an Epoch with timescale T at the given date and time.\n\nExample\n\njulia> Epoch{TT}(2017, 3, 14, 7, 18, 20, 325)\n2017-03-14T07:18:20.325 TT\n\n\n\n"
+},
+
+{
     "location": "api/time.html#",
     "page": "Time",
     "title": "Time",
     "category": "page",
-    "text": ""
+    "text": "DocTestSetup = quote\n    using AstronomicalTime\nendModules = [AstronomicalTime]DocTestSetup = nothing"
 },
 
 ]}
