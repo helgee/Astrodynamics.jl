cf2b77667a36f145e25e944ff91f7c1dc8428cb4
Helge Eichhorn <git@helgeeichhorn.de>
2017-03-23 17:24:07 +0100

    Frame conversion benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,6 +6,7 @@ Astrodynamics.update()
 @benchgroup "frames" ["frames", "conversions"] begin
     rv = ones(6)
     ep = TDBEpoch(2000, 1, 1)
-    rot = Rotation(IAUEarth, ITRF, ep)
-    @bench "multiconv" $rot($rv)
+    @bench "gcrf_itrf" Rotation(GCRF, ITRF, $ep)($rv)
+    @bench "gcrf_iau" Rotation(GCRF, IAUEarth, $ep)($rv)
+    @bench "iau_itrf" Rotation(IAUEarth, ITRF, $ep)($rv)
 end
