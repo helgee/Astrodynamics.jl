ca772f2cef32181967cddd8ff178d765ad284401
Helge Eichhorn <git@helgeeichhorn.de>
2019-08-29 20:06:43 +0200

    Use AstroBase

--- src/Astrodynamics.jl
+++ src/Astrodynamics.jl
@@ -5,9 +5,7 @@ module Astrodynamics
 using Reexport
 using RemoteFiles
 
-@reexport using AstroDynBase
-@reexport using AstroDynCoordinates
-@reexport using AstroDynIO
+@reexport using AstroBase
 @reexport using AstroDynPlots
 @reexport using AstroDynPropagators
 @reexport using JPLEphemeris
@@ -17,10 +15,16 @@ using RemoteFiles
     de405 = @RemoteFile "https://naif.jpl.nasa.gov/pub/naif/generic_kernels/spk/planets/a_old_versions/de405.bsp"
 end
 
+const EPHEMERIS = Ref{SPK}()
+
+AstroBase.State(s::AbstractState; kwargs...) = State(s, EPHEMERIS[]; kwargs...)
+# AstroBase.KeplerianState(s::AbstractState; kwargs...) = KeplerianState(State(s, EPHEMERIS[], kwargs...))
+AstroDynPropagators.ThirdBody(bodies...) = ThirdBody(EPHEMERIS[], collect(bodies))
+
 function __init__()
-    AstroDynBase.update()
+    AstroTime.update()
     download(ephemerides)
-    load_ephemeris!(SPK, path(ephemerides, :de430))
+    EPHEMERIS[] = SPK(path(ephemerides, :de430))
 end
 
 end
